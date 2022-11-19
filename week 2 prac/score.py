3165b83304b705801ed352eed6c5942d8d15a1b2
ZERO <handong.liu@my.jcu.edu.au>
2022-11-19 12:53:24 +0800

    prac2

--- week 2 prac/score.py
+++ week 2 prac/score.py
@@ -1,11 +1,18 @@
 # Handong Liu
 
-score = float(input("Enter score: "))
-if score < 0 or score >100:
-    print("Invalid score")
-elif score >= 90:
-            print("Excellent")
-elif score >= 50:
-            print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    print(determine_status(score))
+
+def determine_status(score):
+
+    if score < 0 or score >100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+main()
