c34ee9388b987ddfd8795feba69c20ff72706035
ZERO <handong.liu@my.jcu.edu.au>
2022-12-25 19:10:34 +0800

    prac7

---
+++ prac_07/myguitar.py
@@ -0,0 +1,24 @@
+
+def main():
+
+    class Guitar:
+        def __init__(self, name="", year=0, cost=0.0):
+            self.name = name
+            self.year = year
+            self.cost = cost
+
+    guitar_list = []
+
+    # Read data from file
+    with open("guitars.csv") as file:
+        for line in file:
+            data = line.strip().split(',')
+            name = data[0]
+            year = data[1]
+            cost = data[2]
+            guitar_list.append(Guitar(name, year, cost))
+
+    # Display the data
+    for guitar in guitar_list:
+        print("Guitar Name: {}, Year: {}, Cost: ${}".format(guitar.name, guitar.year, guitar.cost))
+main()
